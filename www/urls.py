4cdba5657062bd9b1770292133d872d067bab3d4
ixjx <vissky1@163.com>
2014-06-26 21:15:11 +0800

    d7 test

--- www/urls.py
+++ www/urls.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 #urls.py
-import transwarp.web
-import models
+from transwarp.web import get, view
+from models import User, Blog, Comment
 
 @view('test_users.html')
 @get('/')
